d9ebaa74f0894019d83fdc26fd933a0269dc1caf
jcg <jcg.cristel@gmail.com>
2022-05-27 21:43:37 -0400

    added reset to form when list item is created

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,13 +29,19 @@ var taskFormHandler = function() {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput) {
+        alert("Please fill out task form.");
+        return;
+    }
+        
     // package data as an obj
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     }
-
+    
     createTaskEl(taskDataObj);
+    formEl.reset();
 }
 
 // listeners
